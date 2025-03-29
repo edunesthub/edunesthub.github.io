cbaf3c34b65daaca98f7cc7d6247c0071db04b76
havoschools <havoagenciesltd@gmail.com>
2025-03-29 13:24:57 +0000

    c

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,3 @@
-importScripts("https://cdn.onesignal.com/sdks/OneSignalSDK.js");
-
 const CACHE_NAME = "Chawp-cache-v3";
 const urlsToCache = [
     "/",
@@ -56,44 +54,5 @@ self.addEventListener("fetch", event => {
                     return fetchResponse;
                 })
             ).catch(() => caches.match("/index.html"))
-    );
-});
-
-// Push Notification Event - Handle OneSignal notifications
-self.addEventListener("push", event => {
-    let data = {};
-    if (event.data) {
-        data = event.data.json();
-    }
-
-    const title = data.headings?.en || "Chawp Update";
-    const options = {
-        body: data.contents?.en || "Something new, fam!",
-        icon: "/img/icon-192x192.png",
-        badge: "/img/fav.png",
-        vibrate: [200, 100, 200],
-        data: { url: data.url || "https://chawp.me/index.html" }
-    };
-
-    event.waitUntil(self.registration.showNotification(title, options));
-});
-
-// Notification Click Event - Open the correct URL
-self.addEventListener("notificationclick", event => {
-    event.notification.close();
-    const url = event.notification.data.url || "https://chawp.me/index.html";
-
-    event.waitUntil(
-        clients.matchAll({ type: "window", includeUncontrolled: true })
-            .then(clientList => {
-                for (const client of clientList) {
-                    if (client.url.includes(url) && "focus" in client) {
-                        return client.focus();
-                    }
-                }
-                if (clients.openWindow) {
-                    return clients.openWindow(url);
-                }
-            })
     );
 });
